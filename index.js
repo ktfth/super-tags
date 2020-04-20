a978f8d0b19f5925d6bf0d239d991c38fa46fab2
Kaique Silva <kaeyosthaeron@gmail.com>
2020-04-20 13:49:58 -0300

    Covered the class and id

--- index.js
+++ index.js
@@ -7,3 +7,33 @@ function expandAbbreviationHandler(p='', v='') {
   return `<${p}></${p}>`;
 }
 root.expandAbbreviation = expandAbbreviationHandler;
+
+function isClassAttr(v='') { return v.indexOf('.') > -1; }
+
+function produceClass(v='') {
+  let g = v.split('.').filter(v => v !== '').join(' ').trim();
+  return `class="${g}"`;
+}
+
+function isIdAttr(v='') { return v.indexOf('#') > -1; }
+
+function produceId(v='') {
+  let out = '';
+  let g = v.split('#').filter(v => v !== '');
+  if (g.length === 1) {
+    g = g.join(' ').trim();
+    out = `id="${g}"`;
+  }
+  return out;
+}
+
+function expandAttributeHandler(a) {
+  let out = '';
+  if (isClassAttr(a)) {
+    out = produceClass(a);
+  } else if (isIdAttr(a)) {
+    out = produceId(a);
+  }
+  return out;
+}
+root.expandAttribute = expandAttributeHandler;
--- index.test.js
+++ index.test.js
@@ -11,4 +11,24 @@ describe('Abbreviation', () => {
   it('should be expand to a tag with an value', () => {
     assert.equal(lib.expandAbbreviation('div', 'some-text'), '<div>\n\xa0\xa0some-text\n</div>')
   });
+
+  it('should be expand class attribute', () => {
+    assert.equal(lib.expandAttribute('.some-class'), 'class="some-class"');
+  });
+
+  it('should be expand class attribute with two or more values', () => {
+    assert.equal(
+      lib.expandAttribute('.first-class.second-class'),
+      'class="first-class second-class"'
+    );
+
+    assert.equal(
+      lib.expandAttribute('.first-class.second-class.third-class'),
+      'class="first-class second-class third-class"'
+    );
+  });
+
+  it('should be expand id attribute', () => {
+    assert.equal(lib.expandAttribute('#some-id'), 'id="some-id"');
+  });
 });
